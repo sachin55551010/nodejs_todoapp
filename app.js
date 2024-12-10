026d736e48c839804bbfd9b932096812e1f22c08
Sachin Khatri <sachinbansal3333@gmail.com>
2024-12-10 14:43:28 +0530

    forntend server added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
